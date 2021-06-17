a3d238fd6fdde8f620ab4f61513333302529dee0
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-06-17 13:14:53 +0530

    phase 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+
+      <input type="checkbox" />
+      <p>placeholder text</p>
+      <input type="checkbox" />
+      <p>placeholder text</p>
+      <input type="checkbox" />
+      <p>placeholder text</p>
+      
+    </div>
+  );
+}
+
+export default App;
